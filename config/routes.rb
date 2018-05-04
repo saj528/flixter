939eae58e06da6f7cfa89e3e8ecfec992d75aa09
saj528 <ajmarksboy@gmail.com>
2018-05-04 16:52:23 +0000

    Course creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :static_pages
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end
