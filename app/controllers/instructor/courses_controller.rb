499c3e9f1153168bb41039ac57855d2a91a3596e
saj528 <ajmarksboy@gmail.com>
2018-05-13 00:56:57 +0000

    Lesson 12

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -8,33 +8,16 @@ class Instructor::CoursesController < ApplicationController
 
   def create
     @course = current_user.courses.create(course_params)
-    if @course.valid?
-      redirect_to instructor_course_path(@course)
-    else
-      render :new, status: :unprocessable_entity
-    end
+    redirect_to instructor_course_path(@course)
   end
 
   def show
-    @section = Section.new
-    @lesson = Lesson.new
+    @course = Course.find(params[:id])
   end
-  
+
   private
-
-  helper_method :current_course
-  def current_course
-    @current_course ||= Course.find(params[:id])
-  end
-
-  def require_authorized_for_current_course
-    if current_course.user != current_user
-      render plain: "Unauthorized", status: :unauthorized
-    end
-  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
   end
 end
-
--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -3,6 +3,10 @@ class Instructor::LessonsController < ApplicationController
   before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
 
+  def new
+    @section = Section.find(params[:section_id])
+  end
+
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
