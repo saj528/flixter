98f832cbfc3d80bfda1802969589cb8c3b5ff71f
saj528 <ajmarksboy@gmail.com>
2018-05-13 01:56:00 +0000

    ls18

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -5,43 +5,19 @@ class Instructor::LessonsController < ApplicationController
 
   def new
     @section = Section.find(params[:section_id])
+    @lesson = Lesson.new
   end
 
 
   def create
-    @lesson = current_section.lessons.create(lesson_params)
-    redirect_to instructor_course_path(current_section.course)
-  end
-  
-  def update
-    current_lesson.update_attributes(lesson_params)
-    render plain: 'updated!'
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(@section.course)
   end
 
   private
-  
-  def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
-  
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
-
-  def require_authorized_for_current_section
-    if current_section.course.user != current_user
-      return render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
-
-  helper_method :current_section
-  def current_section
-    @current_section ||= Section.find(params[:section_id])
-  end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
+    params.require(:lesson).permit(:title, :subtitle)
   end
 end
