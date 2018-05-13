c93e41fe7f3d129d6398db1bcb2ae94533c1f6e6
saj528 <ajmarksboy@gmail.com>
2018-05-13 02:13:49 +0000

    ls20

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -21,6 +21,17 @@ class Instructor::CoursesController < ApplicationController
 
   private
 
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
   def course_params
     params.require(:course).permit(:title, :description, :cost)
   end
