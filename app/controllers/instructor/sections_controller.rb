5bb749bc990824e3348dff74a3cb2d92a866ae9c
saj528 <ajmarksboy@gmail.com>
2018-05-13 01:49:55 +0000

    ls17

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -3,42 +3,19 @@ class Instructor::SectionsController < ApplicationController
   before_action :require_authorized_for_current_course
 
   def new
+    @course = Course.find(params[:course_id])
     @section = Section.new
   end
 
   def create
-    @section = current_course.sections.create(section_params)
-    redirect_to instructor_course_path(current_course)
+    @course = Course.find(params[:course_id])
+    @section = @course.sections.create(section_params)
+    redirect_to instructor_course_path(@course)
   end
 
   private
 
-  def require_authorized_for_current_course
-    if current_course.user != current_user
-      render plain: "Unauthoirzed", status: :unauthorized
-    end
-  end
-
-  def require_authorized_for_current_section
-    if current_section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
-
-  helper_method :current_course
-  def current_course
-    if params[:course_id]
-      @current_course ||= Course.find(params[:course_id])
-    else
-      current_section.course
-    end
-  end
-
-  def current_section
-    @section ||= Section.find(params[:id])
-  end
-
   def section_params
-    params.require(:section).permit(:title, :row_order_position)
+    params.require(:section).permit(:title)
   end
 end
