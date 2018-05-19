894d08a6355ee967b0e09b28148399f21e61b00f
saj528 <ajmarksboy@gmail.com>
2018-05-19 14:58:22 +0000

    enroll auth

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,18 @@
 class LessonsController < ApplicationController
   
+  before_action :authenticate_user!
+  before_action :user_enrolled!
+
   def show
   end
-  
+
   private
+
+  def user_enrolled!
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to :back, :alert => "You must be enrolled to view lesson!"
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
